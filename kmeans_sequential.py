09b289a7650e31a892aeac297ddd295990558cb6
Timur Turushev <ta.turushev@gmail.com>
2024-12-27 11:58:09 +0300

    remove commented code

--- kmeans_sequential.py
+++ kmeans_sequential.py
@@ -22,14 +22,6 @@ class KMeansSequential:
                 X[labels == k].mean(axis=0)
                 for k in range(self.n_clusters)
             ])
-            #new_centroids = np.zeros((self.n_clusters, X.shape[1]),dtype=np.float64)
-            #for i in range(self.n_clusters):
-            #    xk = X[labels == i]
-            #    summ = np.zeros((X.shape[1]), dtype=np.float64)
-            #    for j in range(xk.shape[0]):
-            #        summ += xk[j]
-            #    new_centroids[i] = summ/xk.shape[0]
-            #print(new_centroids)
             if np.linalg.norm(new_centroids - centroids) < self.tol:
                 break
 
