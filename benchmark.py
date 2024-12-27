0754e30b3ba466344f0758b7a34cf7f6b5af0cb3
Timur Turushev <ta.turushev@gmail.com>
2024-12-27 03:03:39 +0300

    experiment setup

--- benchmark.py
+++ benchmark.py
@@ -6,14 +6,15 @@ from sklearn.datasets import make_blobs
 from kmeans_sequential import KMeansSequential
 from kmeans_parallel import KMeansCuda
 
-SEED = 1111
+SEED = 0
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please specify number of workers as argument")
         sys.exit(1)
+    print(f"Experiment with {sys.argv[1]}x{sys.argv[1]} threads per block in CUDA")
     # Generate data
-    X, _ = make_blobs(n_samples=70000, n_features=50, centers=5, center_box=(1,1), random_state=SEED)
+    X, _ = make_blobs(n_samples=70000, n_features=50, centers=5, random_state=SEED)
 
     np.random.seed(SEED)
     kmeans_seq = KMeansSequential(max_iter=1000)
@@ -33,13 +34,3 @@ if __name__ == "__main__":
     print(f"Parallel KMeans execution time (s): {elapsed_parallel}")
     print(f"Correctnes check: {np.allclose(centroids_seq,centroids_para)}")
     print(f"Speedup (sequential_time/parallel_time): {elapsed_seq/elapsed_parallel}")
-    #for n_threads in [4, 8, 16, 32]:
-    #    np.random.seed(SEED)
-    #    kmeans_parallel = KMeansCuda(n_threads=n_threads)
-    #    t0 = time.perf_counter()
-    #    centroids_para, _ = kmeans_parallel.fit(X)
-    #    t1 = time.perf_counter()
-    #    elapsed_parallel = t1 - t0
-    #    print(f"Parallel KMeans with {n_threads}x{n_threads} threads per block execution time (s): {elapsed_parallel}")
-    #    print(f"Correctnes check: {np.allclose(centroids_seq,centroids_para)}")
-
---
+++ experiment.sh
@@ -0,0 +1,4 @@
+python benchmark.py 4
+python benchmark.py 8
+python benchmark.py 16
+python benchmark.py 32
